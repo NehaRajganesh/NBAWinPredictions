661ea53163fdba13efc2755b052b2687c49d55ba
NehaRajganesh <neha.rajganesh@gmail.com>
2025-04-28 18:35:07 -0500

    test playoff data

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # My first app
 Hello *World!*
 """)
+
+# Loading in data
+df = pd.read_csv('/Users/neharajganesh/Desktop/Projects/NBAWinPredictions/data/playoff.csv')
+
+#title
+st.title('NBA Playoff Games Viewer')
+
+#Pick a date
+selected_date = st.date_input('Select a playoff game date')
+
+# datetime format 
+df['Date'] = pd.to_datetime(df['Date'])
+
+# filtering games by the selected date
+filtered_games = df[df['Date'] == pd.to_datetime(selected_date)]
+
+# Display
+if not filtered_games.empty:
+    st.write(f"Playoff games on {selected_date}:")
+    st.dataframe(filtered_games)
+else:
+    st.write(f"No playoff games found for {selected_date}.")
